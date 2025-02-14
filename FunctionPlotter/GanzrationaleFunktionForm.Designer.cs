61ad8fd4214202fc0cc29fbe6fdda7daa9771f48
Yahya Erbasaran <yahyaerbasaran07@gmail.com>
2025-02-14 18:47:16 +0100

    Ganzrationale Fkt. überarbeitet und fertig.

--- FunctionPlotter/GanzrationaleFunktionForm.Designer.cs
+++ FunctionPlotter/GanzrationaleFunktionForm.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelKoeffizienten = new System.Windows.Forms.Label();
+            this.buttonZeichnen = new System.Windows.Forms.Button();
+            this.textBoxKoeffizienten = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelKoeffizienten
+            // 
+            this.labelKoeffizienten.AutoSize = true;
+            this.labelKoeffizienten.Location = new System.Drawing.Point(12, 29);
+            this.labelKoeffizienten.Name = "labelKoeffizienten";
+            this.labelKoeffizienten.Size = new System.Drawing.Size(226, 16);
+            this.labelKoeffizienten.TabIndex = 0;
+            this.labelKoeffizienten.Text = "Koeffizienten (durch Komma getrennt)";
+            // 
+            // buttonZeichnen
+            // 
+            this.buttonZeichnen.Location = new System.Drawing.Point(15, 82);
+            this.buttonZeichnen.Name = "buttonZeichnen";
+            this.buttonZeichnen.Size = new System.Drawing.Size(75, 23);
+            this.buttonZeichnen.TabIndex = 1;
+            this.buttonZeichnen.Text = "Zeichnen";
+            this.buttonZeichnen.UseVisualStyleBackColor = true;
+            this.buttonZeichnen.Click += new System.EventHandler(this.buttonZeichnen_Click);
+            // 
+            // textBoxKoeffizienten
+            // 
+            this.textBoxKoeffizienten.Location = new System.Drawing.Point(300, 26);
+            this.textBoxKoeffizienten.Name = "textBoxKoeffizienten";
+            this.textBoxKoeffizienten.Size = new System.Drawing.Size(140, 22);
+            this.textBoxKoeffizienten.TabIndex = 2;
+            // 
+            // GanzrationaleFunktionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PolynomialFunctionForm";
+            this.Controls.Add(this.textBoxKoeffizienten);
+            this.Controls.Add(this.buttonZeichnen);
+            this.Controls.Add(this.labelKoeffizienten);
+            this.Name = "GanzrationaleFunktionForm";
+            this.Text = "Ganzrationale Funktion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelKoeffizienten;
+        private System.Windows.Forms.Button buttonZeichnen;
+        private System.Windows.Forms.TextBox textBoxKoeffizienten;
     }
 }
--- FunctionPlotter/GanzrationaleFunktionForm.cs
+++ FunctionPlotter/GanzrationaleFunktionForm.cs
@@ -19,45 +19,38 @@ namespace FunktionZeichnen
             InitializeComponent(); 
             Text = "Ganzrationale Funktion";// Titel des Fensters setzen
 
-            // UI-Elemente erstellen
-            var labelKoeffizienten = new Label() { Text = "Koeffizienten (durch Komma getrennt):", Top = 20, Left = 20 }; // Label für die Koeffizienteneingabe
-            var textBoxKoeffizienten = new TextBox() { Top = 20, Left = 200, Width = 220 };// Eingabefeld für Koeffizienten
-            var buttonZeichnen = new Button() { Text = "Zeichnen", Top = 60, Left = 20 };// Button zum Zeichnen der Funktion
-
-
             // GraphPanel zur Darstellung der Funktion hinzufügen
             _graphPanel = new Koordinatensystem() { Top = 100, Left = 20, Width = 800, Height = 600 };
             Controls.Add(_graphPanel);
-
-            // Event-Handler für den Button-Klick
-            buttonZeichnen.Click += (sender, e) =>
-            {
-                try
-                {
-                    // Eingabe parsen: Trennen nach Kommas, Leerzeichen entfernen und in Double umwandeln
-                    double[] koeffizienten = textBoxKoeffizienten.Text
-                        .Split(',')
-                        .Select(c => double.Parse(c.Trim()))
-                        .ToArray();
-
-                    // Erstellung der Polynomfunktion mit den eingegebenen Koeffizienten
-                    var polynomFunktion = new GanzrationaleFunktion(koeffizienten);
-                    _graphPanel.SetFunktion(polynomFunktion);// Funktion im GraphPanel setzen
-
-                    // Nachricht mit der gezeichneten Funktion anzeigen
-                    MessageBox.Show($"Funktion gezeichnet: y = {string.Join(" + ", koeffizienten.Select((c, i) => $"{c}x^{koeffizienten.Length - 1 - i}"))}");
-                }
-                catch
-                {
-                    // Fehlermeldung, falls die Eingabe ungültig ist
-                    MessageBox.Show("Ungültige Eingabe. Bitte geben Sie numerische Werte ein, getrennt durch Kommas.");
-                }
-            };
 
             // UI-Elemente dem Formular hinzugefügt
             Controls.Add(labelKoeffizienten);
             Controls.Add(textBoxKoeffizienten);
             Controls.Add(buttonZeichnen);
+        }
+
+        private void buttonZeichnen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Eingabe parsen: Trennen nach Kommas, Leerzeichen entfernen und in Double umwandeln
+                double[] koeffizienten = textBoxKoeffizienten.Text
+                    .Split(',')
+                    .Select(c => double.Parse(c.Trim()))
+                    .ToArray();
+
+                // Erstellung der Polynomfunktion mit den eingegebenen Koeffizienten
+                var polynomFunktion = new GanzrationaleFunktion(koeffizienten);
+                _graphPanel.SetFunktion(polynomFunktion);// Funktion im GraphPanel setzen
+
+                // Nachricht mit der gezeichneten Funktion anzeigen
+                MessageBox.Show($"Funktion gezeichnet: y = {string.Join(" + ", koeffizienten.Select((c, i) => $"{c}x^{koeffizienten.Length - 1 - i}"))}");
+            }
+            catch
+            {
+                // Fehlermeldung, falls die Eingabe ungültig ist
+                MessageBox.Show("Ungültige Eingabe. Bitte geben Sie numerische Werte ein, getrennt durch Kommas.");
+            }
         }
     }
 }
