ff3b49c187497e1a4b1e4056bec6ef59d175b575
Yahya Erbasaran <yahyaerbasaran07@gmail.com>
2025-02-14 15:28:38 +0100

    MainForm überarbeitet, UI-Elemente gelöscht und mit MenuStripe die formulare sichtbar gemacht.

--- FunctionPlotter/MainForm.Designer.cs
+++ FunctionPlotter/MainForm.Designer.cs
@@ -28,21 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funktionsFormulareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineareFktToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.qudratischeFktToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exponentialeFktToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ganzrationaleFktToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beendenToolStripMenuItem,
+            this.funktionsFormulareToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.beendenToolStripMenuItem.Text = "beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // funktionsFormulareToolStripMenuItem
+            // 
+            this.funktionsFormulareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineareFktToolStripMenuItem,
+            this.qudratischeFktToolStripMenuItem,
+            this.exponentialeFktToolStripMenuItem,
+            this.ganzrationaleFktToolStripMenuItem});
+            this.funktionsFormulareToolStripMenuItem.Name = "funktionsFormulareToolStripMenuItem";
+            this.funktionsFormulareToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.funktionsFormulareToolStripMenuItem.Text = "Funktions Formulare";
+            // 
+            // lineareFktToolStripMenuItem
+            // 
+            this.lineareFktToolStripMenuItem.Name = "lineareFktToolStripMenuItem";
+            this.lineareFktToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.lineareFktToolStripMenuItem.Text = "Lineare Fkt.";
+            this.lineareFktToolStripMenuItem.Click += new System.EventHandler(this.lineareFktToolStripMenuItem_Click);
+            // 
+            // qudratischeFktToolStripMenuItem
+            // 
+            this.qudratischeFktToolStripMenuItem.Name = "qudratischeFktToolStripMenuItem";
+            this.qudratischeFktToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.qudratischeFktToolStripMenuItem.Text = "Quadratische Fkt.";
+            this.qudratischeFktToolStripMenuItem.Click += new System.EventHandler(this.qudratischeFktToolStripMenuItem_Click);
+            // 
+            // exponentialeFktToolStripMenuItem
+            // 
+            this.exponentialeFktToolStripMenuItem.Name = "exponentialeFktToolStripMenuItem";
+            this.exponentialeFktToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exponentialeFktToolStripMenuItem.Text = "Exponentiale Fkt.";
+            this.exponentialeFktToolStripMenuItem.Click += new System.EventHandler(this.exponentialeFktToolStripMenuItem_Click);
+            // 
+            // ganzrationaleFktToolStripMenuItem
+            // 
+            this.ganzrationaleFktToolStripMenuItem.Name = "ganzrationaleFktToolStripMenuItem";
+            this.ganzrationaleFktToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ganzrationaleFktToolStripMenuItem.Text = "Ganzrationale Fkt.";
+            this.ganzrationaleFktToolStripMenuItem.Click += new System.EventHandler(this.ganzrationaleFktToolStripMenuItem_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem funktionsFormulareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineareFktToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem qudratischeFktToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exponentialeFktToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ganzrationaleFktToolStripMenuItem;
     }
 }
 
--- FunctionPlotter/MainForm.cs
+++ FunctionPlotter/MainForm.cs
@@ -15,42 +15,39 @@ namespace FunktionZeichnen
         public MainForm()
         {
             InitializeComponent();
-            Text = "FunktionsFormulare";
-            IsMdiContainer = true;
-
-            // Menüleiste erstellen
-            var menuStrip = new MenuStrip();
-
-            // Menü "Funktionen"
-            var funktionsMenu = new ToolStripMenuItem("Funktion");
-            var linearMenuItem = new ToolStripMenuItem("Lineare Funktion");
-            var quadratischeMenuItem = new ToolStripMenuItem("Quadratische Funktion");
-            var exponentialMenuItem = new ToolStripMenuItem("Exponentiale Funktion");
-            var ganzrationaleMenuItem = new ToolStripMenuItem("Ganzrationale Funktion");
-
-            // Menü-Events
-            linearMenuItem.Click += (s, e) => OpenForm(new LinearFunktionForm());
-            quadratischeMenuItem.Click += (s, e) => OpenForm(new QuadratischeFunktionForm());
-            exponentialMenuItem.Click += (s, e) => OpenForm(new ExponentialFunktionForm());
-            ganzrationaleMenuItem.Click += (s, e) => OpenForm(new GanzrationaleFunktionForm());
-
-            // Menüpunkte hinzufügen
-            funktionsMenu.DropDownItems.Add(linearMenuItem);
-            funktionsMenu.DropDownItems.Add(quadratischeMenuItem);
-            funktionsMenu.DropDownItems.Add(exponentialMenuItem);
-            funktionsMenu.DropDownItems.Add(ganzrationaleMenuItem);
-
-            // Menüleiste hinzufügen
-            menuStrip.Items.Add(funktionsMenu);
-            MainMenuStrip = menuStrip;
-            Controls.Add(menuStrip);
+        }
+        private void lineareFktToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new LinearFunktionForm();
+            form.Show();
         }
 
-        private void OpenForm(Form form)
+        private void qudratischeFktToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            form.WindowState = FormWindowState.Maximized;
-            form.MdiParent = this;
+            var form = new QuadratischeFunktionForm();
             form.Show();
+        }
+
+        private void exponentialeFktToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new ExponentialFunktionForm();
+            form.Show();
+        }
+
+        private void ganzrationaleFktToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new GanzrationaleFunktionForm();
+            form.Show();
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
