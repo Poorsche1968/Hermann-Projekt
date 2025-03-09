de34321bdef7ec751e81451b47a348ead4114d71
Poorsche1968 <Michel-Scholer@gmx.de>
2025-03-09 19:40:55 +0100

    Funktion.cs überarbeitet. kommentare eingefügt und regions eingefügt

--- FunctionPlotter/Funktion.cs
+++ FunctionPlotter/Funktion.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace FunktionZeichnen
+﻿namespace FunktionZeichnen
 {
-    // Abstrakte Basisklasse für mathematische Funktionen
+    //Abstrakte Basisklasse für mathematische Funktionen
     public abstract class Funktion
     {
-        // Abstrakte Methode zur Berechnung des Funktionswertes an einer bestimmten Stelle x
+        #region Abstrakte Methode zur Berechnung des Funktionswertes
+
+        // Diese Methode muss in abgeleiteten Klassen überschrieben werden.
+        // Sie nimmt einen x-Wert als Eingabe und gibt den berechneten y-Wert zurück.
         public abstract double Berechne(double x);
 
-        // Methode zur Rückgabe des Funktionstyps als String (kann in Unterklassen überschrieben werden)
+        #endregion
+
+        #region Virtuelle Methode zur Funktionsbeschreibung
+
+        // Gibt den Typ der Funktion zurück.
+        // Diese Methode kann in abgeleiteten Klassen überschrieben werden, um genauere Bezeichnungen zu liefern.
         public virtual string AuswhalFunktionsTyp()
         {
-            return "Basisfunktion";//Gibt den die Basis Funktion zurück
+            return "Basisfunktion"; // Standardwert, falls nicht überschrieben
         }
+
+        #endregion
     }
 }
