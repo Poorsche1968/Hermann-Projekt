22d4fb8a633ba7009a47d59cb5e2ed29fbed6e76
Poorsche1968 <Michel-Scholer@gmx.de>
2025-03-09 19:09:24 +0100

    umbenennung label 1

--- FunctionPlotter/GanzrationaleFunktionForm.Designer.cs
+++ FunctionPlotter/GanzrationaleFunktionForm.Designer.cs
@@ -91,9 +91,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(81, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "Ausgabe Funktion:";
             // 
             // label2
             // 
