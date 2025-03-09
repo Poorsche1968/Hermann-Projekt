e42588856891f0c9a90f8e36662fb9d40aeda316
Poorsche1968 <Michel-Scholer@gmx.de>
2025-03-09 19:59:34 +0100

    GanzrationaleFunktion : Funktion Kommentare und #region eingefügt

--- FunctionPlotter/Funktion.cs
+++ FunctionPlotter/Funktion.cs
@@ -5,19 +5,19 @@
     {
         #region Abstrakte Methode zur Berechnung des Funktionswertes
 
-        // Diese Methode muss in abgeleiteten Klassen überschrieben werden.
-        // Sie nimmt einen x-Wert als Eingabe und gibt den berechneten y-Wert zurück.
+        //Diese Methode muss in abgeleiteten Klassen überschrieben werden.
+        //Sie nimmt einen x-Wert als Eingabe und gibt den berechneten y-Wert zurück.
         public abstract double Berechne(double x);
 
         #endregion
 
         #region Virtuelle Methode zur Funktionsbeschreibung
 
-        // Gibt den Typ der Funktion zurück.
-        // Diese Methode kann in abgeleiteten Klassen überschrieben werden, um genauere Bezeichnungen zu liefern.
+        //Gibt den Typ der Funktion zurück.
+        //Diese Methode kann in abgeleiteten Klassen überschrieben werden, um genauere Bezeichnungen zu liefern.
         public virtual string AuswhalFunktionsTyp()
         {
-            return "Basisfunktion"; // Standardwert, falls nicht überschrieben
+            return "Basisfunktion"; //Standardwert, falls nicht überschrieben
         }
 
         #endregion
--- FunctionPlotter/GanzrationaleFunktion.cs
+++ FunctionPlotter/GanzrationaleFunktion.cs
@@ -1,37 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FunktionZeichnen
 {
-    // Klasse zur Darstellung einer allgemeinen Ganzrationalefunktion
+    //Klasse zur Darstellung einer allgemeinen Ganzrationalefunktion
     public class GanzrationaleFunktion : Funktion
     {
-        private double[] _koeffizienten;// Array zur Speicherung der Koeffizienten
+        #region Eigenschaften
 
-        // Konstruktor: Initialisiert die Ganzrationalefunktion mit den gegebenen Koeffizienten
+        //Array zur Speicherung der Koeffizienten der Ganzrationalen Funktion
+        private double[] _koeffizienten;
+
+        #endregion
+
+        #region Konstruktor
+
+        //Initialisiert die Ganzrationale Funktion mit den gegebenen Koeffizienten
         public GanzrationaleFunktion(double[] koeffizienten)
         {
             _koeffizienten = koeffizienten;
         }
-        
-        // Methode zur Berechnung des Funktionswertes an einer bestimmten Stelle x
+
+        #endregion
+
+        #region Methoden
+
+        //Berechnet den Funktionswert an einer bestimmten Stelle x
         public override double Berechne(double x)
         {
-            double result = 0;
+            double ergebnis = 0;
+
+            //Iteriert über alle Koeffizienten und berechnet den Funktionswert mit der Potenzregel für Polynome
             for (int i = 0; i < _koeffizienten.Length; i++)
             {
-                // Berechnung des Funktionswerts mit der Potenzregel für Polynome
-                result += _koeffizienten[i] * Math.Pow(x, _koeffizienten.Length - 1 - i);
+                //Berechnung des Funktionswerts mit der Potenzregel: a_n * x^n + a_(n-1) * x^(n-1) + ... + a_1 * x + a_0
+                ergebnis += _koeffizienten[i] * Math.Pow(x, _koeffizienten.Length - 1 - i);
             }
-            return result;
+
+            return ergebnis;
         }
 
-        // Methode zur Rückgabe des Funktionstyps als String
+        //Gibt den Funktionstyp als String zurück
         public override string AuswhalFunktionsTyp()
         {
-            return "Ganzrationale Funktion";// Deutsche Bezeichnung für die Funktion
+            return "Ganzrationale Funktion"; //Rückgabe der Bezeichnung der Funktion
         }
+
+        #endregion
     }
 }
